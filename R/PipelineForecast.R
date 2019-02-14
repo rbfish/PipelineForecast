4ede044ee199cdfacfa408e6a98c9a045d5646fa
Rick Fisher <34216948+rbfish@users.noreply.github.com>
2019-02-14 07:46:18 -0500

    Initial commit for project code.

---
+++ R/PipelineForecast.R
@@ -0,0 +1,18 @@
+library(arcgisbinding)
+library(sf)
+library(tidyverse)
+
+# Check connection to ArcGIS Pro
+arc.check_product()
+
+# Import data sets from geodatabase into R using ArcGIS Bridge
+
+permit_df <- arc.open(path = 'C:/RStats/PipelineForecast/Data/Pipeline.gdb/ResidentialBuildingPermit')
+
+project_df <- arc.open(path = 'C:/RStats/PipelineForecast/Data/Pipeline.gdb/ResidentialDevelopmentActivity')
+
+# Verify data has been correctly loaded into R
+
+head(permit_df)
+
+head(project_df)
