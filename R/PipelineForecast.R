09b06e3d211b1662709acc66d76ce80e5bf0b305
Rick Fisher <34216948+rbfish@users.noreply.github.com>
2019-02-21 11:56:06 -0500

    Made comment refinements

--- R/PipelineForecast.R
+++ R/PipelineForecast.R
@@ -24,7 +24,7 @@ permitProject_df <- st_intersection(permit_df, project_df)
 
 glimpse(permitProject_df)
 
-# Testing tidyverse code to count permits per project number
+# Testing tidyverse code to count monthly permits per project number
 
 test1 <- permitProject_df %>% 
     filter(CP_IMP_TYP == "NEW") %>%
